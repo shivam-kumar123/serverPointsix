2de08278f18b5d44b92d896d7180eb8e7d7064be
shivam kumar <20051252@kiit.ac.in>
2023-07-09 12:44:52 +0530

    dynamic rendering added

--- server.js
+++ server.js
@@ -36,6 +36,44 @@
 // })
 
 // server.js
+// const express = require('express');
+// const cors = require('cors');
+// const path = require('path');
+// const fs = require('fs');
+
+// const PORT = process.env.PORT || 5000;
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// let imageUrl = 'https://fastly.picsum.photos/id/813/400/400.jpg?hmac=3eUkOPA1X4a9JB_fNq27cSoZ_ii17tUciJnLjDvW7lA';
+
+// app.get('/', (req, res) => {
+//   const filePath = path.resolve(__dirname, './build', 'index.html');
+//   fs.readFile(filePath, 'utf-8', (err, data) => {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     data = data.replace(/__IMAGE__/g, imageUrl);
+
+//     res.send(data);
+//   });
+// });
+
+// app.post('/', (req, res) => {
+//   imageUrl = req.body.imageUrl;
+//   console.log('server received request')
+//   res.sendStatus(200);
+// });
+
+// app.use(express.static(path.resolve(__dirname, './build')));
+
+// app.listen(PORT, () => {
+//   console.log(`Server Started on Port ${PORT}`);
+// });
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -50,6 +88,10 @@ app.use(express.json());
 
 let imageUrl = 'https://fastly.picsum.photos/id/813/400/400.jpg?hmac=3eUkOPA1X4a9JB_fNq27cSoZ_ii17tUciJnLjDvW7lA';
 
+const updateImageUrl = (newImageUrl) => {
+  imageUrl = newImageUrl;
+};
+
 app.get('/', (req, res) => {
   const filePath = path.resolve(__dirname, './build', 'index.html');
   fs.readFile(filePath, 'utf-8', (err, data) => {
@@ -63,9 +105,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  imageUrl = req.body.imageUrl;
-  console.log('server received request')
+  const { imageUrl: newImageUrl } = req.body;
+  updateImageUrl(newImageUrl);
+  console.log('Server received request:', newImageUrl);
   res.sendStatus(200);
+  // Trigger the GET route by redirecting to it
+  res.redirect('/');
 });
 
 app.use(express.static(path.resolve(__dirname, './build')));
